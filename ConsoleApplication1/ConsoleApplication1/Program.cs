e72828761ec1f1eae10b253a4315ca786c6ba189
kabirov08 <kabirov08@yandex.ru>
2016-09-29 15:21:41 +0400

    я кое что доработал

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, my name is Alesha, and this is my lab.");
+            Console.WriteLine("Hi, my name is Alesha, and this is my lab.I did everything and I'm good.");
             Console.ReadLine();
         }
     }
